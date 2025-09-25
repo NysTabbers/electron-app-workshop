3453a9999a37b72d2ae6b9d307dc5f74d4c154c6
Nys Tabbers <Nystabbers2@gmail.com>
2025-09-25 16:37:36 +0200

    adding localstorage not finished

--- js/preload.js
+++ js/preload.js
@@ -3,7 +3,10 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke("ping")
-  // we can also expose variables, not just functions
+  electron: () => process.versions.electron
 })
+
+contextBridge.exposeInMainWorld('electronAPI', {
+  getState: () => ipcRenderer.invoke('get-state'),
+  updateUserInfo: (newState) => ipcRenderer.send('update-userInfo', newState)
+});
